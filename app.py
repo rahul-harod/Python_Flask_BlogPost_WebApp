cc2ae768480bcd793b42ff0cdb767926a5aa45f6
Rahul Harod <56213859+rahul-harod@users.noreply.github.com>
2021-01-06 16:17:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,100 @@
+from flask import Flask,render_template, request,redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import os
+
+# file_path = os.path.abspath(os.getcwd())+"\posts.db"
+app=Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///posts.db'
+db = SQLAlchemy(app)
+
+class BlogPost(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(100),nullable=False)
+    content=db.Column(db.Text,nullable=False)
+    author=db.Column(db.String(20),nullable=False,default='N/A')
+    date_posted=db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
+
+    def __repr__(self):
+        return 'Blog post ' +str(self.id)
+
+all_posts=[
+    {
+        'title':'Men in Dark',
+        'content':'real Life',
+        'author':'Barak Obama'
+},
+   {
+        'title':'Sucusess',
+        'content':'non realistic',
+}
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/posts',methods=['GET','POST'])
+def post():
+
+    if request.method=='POST':
+        post_title=request.form['title']
+        post_content=request.form['content']
+        post_author=request.form['author']
+        new_post=BlogPost(title=post_title,author=post_author,content=post_content)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        all_posts=BlogPost.query.order_by(BlogPost.date_posted).all()
+    return render_template('posts.html',posts=all_posts)
+
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+    blog=BlogPost.query.get_or_404(id)
+    db.session.delete(blog)
+    db.session.commit()
+    return redirect('/posts')
+
+@app.route('/posts/edit/<int:id>',methods=['GET','POST'])
+def edit(id):
+    post=BlogPost.query.get_or_404(id)
+
+    if request.method=='POST':
+        post.title=request.form['title']
+        post.content=request.form['content']
+        post.author=request.form['author']
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('edit.html',post=post)
+
+
+@app.route('/posts/new_post',methods=['GET','POST'])
+def new_post():
+
+    if request.method=='POST':
+        post_title=request.form['title']
+        post_content=request.form['content']
+        post_author=request.form['author']
+        new_post=BlogPost(title=post_title,author=post_author,content=post_content)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        all_posts=BlogPost.query.order_by(BlogPost.date_posted).all()
+    return render_template('create_new_post.html',posts=all_posts)
+
+
+# @app.route('/user/<string:name>')
+# def newone(name):
+#     return "Hellow "+name
+if __name__=='__main__':
+    app.run(debug=True) 
+
+
+
+
+
+
